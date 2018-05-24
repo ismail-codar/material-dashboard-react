e59f9347b3eebc9d9c2d715e53f8bdcea96e4182
ismail codar <icodar@gmail.com>
2018-05-24 13:14:07 +0300

    Typescript support added

--- config-overrides.js
+++ config-overrides.js
@@ -2,5 +2,8 @@ const rewireReactHotLoader = require("react-app-rewire-hot-loader");
 
 module.exports = function override(config, env) {
   config = rewireReactHotLoader(config, env);
+
+  config.module.rules.push({ test: /\.tsx?$/, loader: "ts-loader" });
+
   return config;
 };

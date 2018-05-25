c0cc0a074ef8c61b9fa5cca88e11777f44cdd420
ismail codar <icodar@gmail.com>
2018-05-25 19:08:39 +0300

    MiniCssExtractPlugin

--- webpack.config.js
+++ webpack.config.js
@@ -4,8 +4,11 @@ const CopyPlugin = require("copy-webpack-plugin");
 const CleanWebpackPlugin = require("clean-webpack-plugin");
 const BundleAnalyzerPlugin = require("webpack-bundle-analyzer")
   .BundleAnalyzerPlugin;
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
 const ROOT = path.resolve(__dirname, ".");
 const getRoot = path.join.bind(path, ROOT);
+const devMode = process.env.NODE_ENV !== "production";
 
 module.exports = {
   context: __dirname,
@@ -45,32 +48,19 @@ module.exports = {
     rules: [
       { test: /\.[tj]sx?$/, use: ["babel-loader"] },
       {
-        test: /\.css$/,
-        use: [
-          {
-            loader: "file-loader",
-            options: {
-              name: "assets/[name].[ext]"
-            }
-          },
-          {
-            loader: "extract-loader",
-            options: {
-              publicPath: "../"
-            }
-          },
-          {
-            loader: "css-loader"
-          }
-        ]
-      },
-      {
         test: /\.(ttf2?|eot|svg|png|jpg|gif)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
         loader: "file-loader"
       },
       {
         test: /\.woff(2)?(\?v=\d+\.\d+\.\d+)?$/,
         loader: "url-loader?limit=10000&mimetype=application/font-woff"
+      },
+      {
+        test: /\.s?[ac]ss$/,
+        use: [
+          devMode ? "style-loader" : MiniCssExtractPlugin.loader,
+          "css-loader"
+        ]
       }
     ]
   },
@@ -78,7 +68,7 @@ module.exports = {
   plugins: [
     new FriendlyErrorsWebpackPlugin({ clearConsole: true }),
     // new CopyPlugin([{ from: "src/app/app.html", to: "" }]),
-    new CleanWebpackPlugin("dist", { root: getRoot() })
+    new CleanWebpackPlugin("dist", { root: getRoot() }),
     // new BundleAnalyzerPlugin({
     //   // Can be `server`, `static` or `disabled`.
     //   // In `server` mode analyzer will activate HTTP server to show bundle report.
@@ -89,6 +79,12 @@ module.exports = {
     //   openAnalyzer: true,
     //   logLevel: "info"
     // })
+    new MiniCssExtractPlugin({
+      // Options similar to the same options in webpackOptions.output
+      // both options are optional
+      filename: devMode ? "[name].css" : "[name].[hash].css",
+      chunkFilename: devMode ? "[id].css" : "[id].[hash].css"
+    })
   ],
   devServer: {
     port: 3040,
